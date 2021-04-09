1e020fb530ee74ed42e93dd74c9d06cece2b47c5
GriffinHackley <griffinhackley@yahoo.com>
2021-04-08 20:11:17 -0600

    Changed bust function to be more efficient with MPI

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>  // cout/cin
 #include <mpi.h> 
+#include <math.h> 
 #include "game/game.hpp"
 
 using namespace std;
@@ -20,7 +21,6 @@ using namespace std;
 // 3: Win
 // 4: Lose
 
-//TODO:likelihood of busting on a hit
 //Need: ability to play single round
 double bustOnHit(int rank, int size, int iterations){
     MPI_Status status;
@@ -35,81 +35,86 @@ double bustOnHit(int rank, int size, int iterations){
         int* temp = game.gameInfo();
         int* data = temp;
 
+        int work = floor((double)iterations/(double)(size-1));
+        int leftover = iterations%(size-1);
+
         //send work to all workers
         for(int i = 1; i < size; i++){
+            //send game state
             MPI_Send(data, data[0], MPI_INT, i, 1, MCW);
-            sent++;
-            iterations--;
+            //send amount of work to do
+            MPI_Send(&work, 1, MPI_INT, i, 2, MCW);
         }
 
-        while(iterations > 0){
-            //recieve results
-            MPI_Recv(&result, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MCW, &status);
-            won += result;
+        //do the extra work
+        for(int i = 0; i < leftover; i++){
+            //mute output
+            clog.setstate(ios_base::failbit);
 
-            //send more work to workers
-            MPI_Send(data, data[0], MPI_INT, status.MPI_SOURCE, 1, MCW);
-            sent++;
-            iterations--;
+            //workers will simulate a single round
+            temp[1]=1;
+            Game game = Game(temp);
+            game.play(1);
+
+            //get results
+            if(game.players[0]->state != busted){
+                result = 1;
+            } else {
+                result = 0;
+            }
+            won+=result;
         }
 
-        //kill all processes
         for(int i = 1; i < size; i++){
-            //recieve final results
             MPI_Recv(&result, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MCW, &status);
             won += result;
-
-            //send kill message
-            data = 0;
-            MPI_Send(&data, 1, MPI_INT, status.MPI_SOURCE, 0, MCW);
         }
+        
 
         //calculate results
-        percent = (double)won/(double)sent;
+        percent = (double)won/(double)iterations;
 
         //print results
-        cout << "The simulation did not bust on " << won << "/" << sent << endl; 
+        cout << "The simulation did not bust on " << won << "/" << iterations << endl; 
         cout << "That means it did not bust " << percent*100 << "% of the time" << endl;
 
     } else {
+        int won = 0;
         int length;
         int result;
-        bool keepGoing = true;
 
         //mute output
         clog.setstate(ios_base::failbit);
 
-        while(keepGoing){
-            //check for kill message
-            MPI_Probe(0, MPI_ANY_TAG, MCW, &status);
-            if(status.MPI_TAG == 0){
-                keepGoing = false;
-                // cout << "Process " << rank << " has been killed" << endl;
-                break;
-            }
+        //check for message length
+        MPI_Probe(0, MPI_ANY_TAG, MCW, &status);
+        
+        //recieve gamestate and amount of work
+        MPI_Get_count(&status, MPI_INT, &length);
+        int recieved[length];
+        int work;
+        MPI_Recv(recieved, length, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MCW, MPI_STATUS_IGNORE);
+        MPI_Recv(&work, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MCW, MPI_STATUS_IGNORE);
 
-            //recieve gamestate
-            MPI_Get_count(&status, MPI_INT, &length);
-
-            int recieved[length];
-            MPI_Recv(recieved, length, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MCW, MPI_STATUS_IGNORE);
-
+        while(work > 0){
             //workers will simulate a single round
             recieved[1]=1;
             Game game = Game(recieved);
             game.play(1);
 
             //get results
-            cout << game.players[0]->state << endl;
+            // cout << game.players[0]->state << endl;
             if(game.players[0]->state != busted){
                 result = 1;
             } else {
                 result = 0;
             }
-
-            //send result back to leader
-            MPI_Send(&result, 1, MPI_INT, 0, 0, MCW);
+            won+=result;
+            work--;
         }
+
+        //send result back to leader
+        MPI_Send(&won, 1, MPI_INT, 0, 0, MCW);
     }
                 
     return 0;
@@ -133,15 +138,60 @@ double winWithHand(int rank){
 
 //TODO:Success of given strategy
 //Need:
-double strategy(int rank){
+double strategy(int rank, int size, int iterations){
     if(rank == 0){
-        //send strategy to worker
+        int result;
+        int won = 0;
+        double percent;
+
+        int strategy = 0;
+
+        //send the strategy type and how many simulations to run
+        int data[2] = {strategy, iterations/(size-1)};
+
+        //send workers data
+        for(int i = 1; i < size; i++){
+            MPI_Send(data, 2, MPI_INT, i, 1, MCW);
+        }
+
+        //do the extra work
+        int leftover = iterations%(size-1);
+
+        //recieve results
+        for(int i = 1; i < size; i++){
+            MPI_Recv(&result, 1, MPI_INT, i, MPI_ANY_TAG, MCW, MPI_STATUS_IGNORE);
+            won += result;
+        }
+
+        cout << won+leftover << endl;
+
+        //calculate results
+        percent = (double)won/(double)iterations;
+
+        //print results
+        cout << "The strategy won " << won << "/" << iterations << " times" << endl; 
+        cout << "That means it won " << percent*100 << "% of the time" << endl;
+        
+        
         //recieve results
         //calculate results
     } else {
-        //create random game state
-        //simulate game using only specified strategy
+        int work[2];
+        int won = 0;
+        //recieve work
+        MPI_Recv(work, 2, MPI_INT, 0, MPI_ANY_TAG, MCW, MPI_STATUS_IGNORE);
+
+        //run simulation specified number of times
+        for(int i = 0; i < work[0]; i++){
+            if(i%2 == 0){
+                won++;
+            }
+            //create random game state
+            //simulate game using only specified strategy
+        }
+
         //send results back to leader
+        MPI_Send(&won, 1, MPI_INT, 0, 1, MCW);
     }
     
     return 0;
@@ -157,7 +207,8 @@ int main(int argc, char **argv){
     srand(rank+time(NULL));
 
 
-    bustOnHit(rank, size, 10);
+    bustOnHit(rank, size, 30);
+    // strategy(rank, size, 10);
 
 
     MPI_Finalize();
