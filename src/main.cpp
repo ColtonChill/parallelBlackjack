182d6278c2f9bde5839f45d88347af6b071f183a
GriffinHackley <griffinhackley@yahoo.com>
2021-04-09 12:27:58 -0600

    Finished hand simulation and added configuration section

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>  // cout/cin
+#include <iostream>
 #include <mpi.h> 
 #include <math.h> 
+#include <array>
 #include "game/game.hpp"
 
 using namespace std;
@@ -21,8 +22,7 @@ using namespace std;
 // 3: Win
 // 4: Lose
 
-//Need: ability to play single round
-double bustOnHit(int rank, int size, int iterations){
+void bustOnHit(int rank, int size, int iterations){
     MPI_Status status;
     if(rank == 0){
         int result;
@@ -42,6 +42,7 @@ double bustOnHit(int rank, int size, int iterations){
         for(int i = 1; i < size; i++){
             //send game state
             MPI_Send(data, data[0], MPI_INT, i, 1, MCW);
+
             //send amount of work to do
             MPI_Send(&work, 1, MPI_INT, i, 2, MCW);
         }
@@ -50,14 +51,17 @@ double bustOnHit(int rank, int size, int iterations){
         for(int i = 0; i < leftover; i++){
             //mute output
             clog.setstate(ios_base::failbit);
-
+            
             //workers will simulate a single round
             temp[1]=1;
             Game game = Game(temp);
-            game.play(1);
+
+            //give them two initial cards
+            game.hitme();
+            game.hitme();
 
             //get results
-            if(game.players[0]->state != busted){
+            if(game.hitme()){
                 result = 1;
             } else {
                 result = 0;
@@ -69,7 +73,6 @@ double bustOnHit(int rank, int size, int iterations){
             MPI_Recv(&result, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MCW, &status);
             won += result;
         }
-        
 
         //calculate results
         percent = (double)won/(double)iterations;
@@ -100,11 +103,11 @@ double bustOnHit(int rank, int size, int iterations){
             //workers will simulate a single round
             recieved[1]=1;
             Game game = Game(recieved);
-            game.play(1);
+            game.hitme();
+            game.hitme();
 
             //get results
-            // cout << game.players[0]->state << endl;
-            if(game.players[0]->state != busted){
+            if(game.hitme()){
                 result = 1;
             } else {
                 result = 0;
@@ -116,29 +119,97 @@ double bustOnHit(int rank, int size, int iterations){
         //send result back to leader
         MPI_Send(&won, 1, MPI_INT, 0, 0, MCW);
     }
-                
-    return 0;
 }
 
-//TODO likelihood of winning with given hand
-//Need: ability to generate a specified hand
-double winWithHand(int rank){
+void winWithHand(int rank, int size, int length, int cards[], int iterations){
     if(rank == 0){
-        //send hand to worker
+        int result;
+        int won = 0;
+        double percent;
+
+        //send card info and amount of work to do
+        int * data = cards;
+        int work = floor((double)iterations/(double)(size-1));
+
+        //send workers data
+        for(int i = 1; i < size; i++){
+            MPI_Send(data, length, MPI_INT, i, 1, MCW);
+            MPI_Send(&work, 1, MPI_INT, i, 2, MCW);
+        }
+
+        //create hand
+        Hand hand = Hand(cards[0]);
+        for(int i = 1; i < length; i++){
+            Card card = Card(cards[i]);
+            hand.add(card);
+        }
+
+        //do the extra work
+        int leftover = iterations%(size-1);
+        clog.setstate(ios_base::failbit);
+        for(int i = 0; i < leftover; i++){
+            //create random game state
+            Game game = Game(3);
+
+            //set players hand
+            game.setPlayer(0, hand);
+
+            //simulate game
+            won += game.play();
+        }
+
         //recieve results
+        for(int i = 1; i < size; i++){
+            MPI_Recv(&result, 1, MPI_INT, i, MPI_ANY_TAG, MCW, MPI_STATUS_IGNORE);
+            won += result;
+        }
+
         //calculate results
+        percent = (double)won/(double)iterations;
+
+        //print results
+        cout << "The hand won " << won << "/" << iterations << " times" << endl; 
+        cout << "That means it won " << percent*100 << "% of the time" << endl;
     } else {
-        //generate new game state with specified hand
-        //worker simulates entire game until win/loss
-        //send result back to leader
+        MPI_Status status;
+        int work;
+        int won = 0;
+
+        //get number of cards
+        MPI_Probe(0, 1, MCW, &status);
+        MPI_Get_count(&status, MPI_INT, &length);
+        int cards[length];
+
+        //recieve cards and amount of work to do
+        MPI_Recv(cards, length, MPI_INT, 0, 1, MCW, MPI_STATUS_IGNORE);
+        MPI_Recv(&work, 1, MPI_INT, 0, 2, MCW, MPI_STATUS_IGNORE);
+
+        //create hand
+        Hand hand = Hand(cards[0]);
+        for(int i = 1; i < length; i++){
+            Card card = Card(cards[i]);
+            hand.add(card);
+        }
+
+        //run simulation specified number of times
+        clog.setstate(ios_base::failbit);
+        for(int i = 0; i < work; i++){
+            //create random game state
+            Game game = Game(3);
+
+            //set players hand
+            game.setPlayer(0, hand);
+
+            //simulate game
+            won += game.play();
+        }
+
+        //send results back to leader
+        MPI_Send(&won, 1, MPI_INT, 0, 1, MCW);
     }
-    
-    return 0;
 }
 
-//TODO:Success of given strategy
-//Need:
-double strategy(int rank, int size, int iterations){
+void strategy(int rank, int size, int iterations){
     if(rank == 0){
         int result;
         int won = 0;
@@ -163,53 +234,85 @@ double strategy(int rank, int size, int iterations){
             won += result;
         }
 
-        cout << won+leftover << endl;
-
         //calculate results
         percent = (double)won/(double)iterations;
 
         //print results
         cout << "The strategy won " << won << "/" << iterations << " times" << endl; 
         cout << "That means it won " << percent*100 << "% of the time" << endl;
-        
-        
-        //recieve results
-        //calculate results
+
     } else {
         int work[2];
         int won = 0;
+
         //recieve work
         MPI_Recv(work, 2, MPI_INT, 0, MPI_ANY_TAG, MCW, MPI_STATUS_IGNORE);
 
         //run simulation specified number of times
-        for(int i = 0; i < work[0]; i++){
+        clog.setstate(ios_base::failbit);
+        for(int i = 0; i < work[1]; i++){
             if(i%2 == 0){
                 won++;
             }
+
             //create random game state
-            //simulate game using only specified strategy
+            Game game = Game(3);
+
+            //set player ai
+
+            //simulate game
+            // game.play();
         }
 
         //send results back to leader
         MPI_Send(&won, 1, MPI_INT, 0, 1, MCW);
     }
-    
-    return 0;
 }
 
 int main(int argc, char **argv){
-    int rank, size, buff;
-    int flag = 0;
+    enum simulationType {hit, hand, strat};
+    int rank, size, length;
     MPI_Init(&argc , &argv);
     MPI_Comm_rank(MCW, &rank);
     MPI_Comm_size(MCW, &size);
-    MPI_Request request;
-    srand(rank+time(NULL));
 
 
-    bustOnHit(rank, size, 30);
-    // strategy(rank, size, 10);
+    //////////////////
+    // CONFIG BEGIN //
+    //////////////////
+    
+    //general
+    int iterations = 3000;
+    simulationType simulate = hit; //hit, hand, or strat
+    
+    //hit
 
+    //hand
+    int cards[] = {9,0};
+
+    //strat
+    int strategy = 0;
+
+    //////////////////
+    //  CONFIG END  //
+    //////////////////
+
+    switch(simulate){
+        case hit:
+            cout << "hit" << endl;
+            bustOnHit(rank, size, iterations);
+            break;
+        case hand:
+            cout << "hand" << endl;
+            length = (sizeof(cards)/sizeof(*cards));
+            winWithHand(rank, size, length, cards, iterations);
+            break;
+        case strat:
+            cout << "strat" << endl;
+            strategy(rank, size, iterations);
+        default:
+            cout << "unrecognized simulation" << endl;
+    }
 
     MPI_Finalize();
     return 0;
